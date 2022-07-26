f0c4383b2ebbaf153658f81e83923fe4f610d98c
jaespinoza13 <am5104144@gmail.com>
2022-07-25 20:06:50 -0500

    Actualizacion de registros de usuario con autenticacion

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
 
   app.useGlobalInterceptors(new TimeOutInterceptor());
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
